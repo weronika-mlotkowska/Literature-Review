9e8eb8eab6533d92dd046477cdccc30716348f76
weronika-mlotkowska <w.mlotkowska@gmail.com>
2025-10-10 13:15:49 +0200

    add metadata extraction pipeline, config system, and README documentation

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Paper_Citation_Analysis
-This project analyzes citation patterns among academic papers by extracting reference data from CrossRef API and identifying the most cited papers in a given list of DOIs.
+## Metadata Extraction Pipeline
+
+A modular Python pipeline for extracting publication metadata from the Elsevier Scopus API based on a list of DOIs.
+
+### Features
+- Reads input DOIs from CSV or JSON
+- Queries the Elsevier Scopus API for each DOI
+- Extracts metadata such as:
+    Publication venue
+    Year
+    Title
+    Citation count
+- Writes results to JSON output
+
+### Usage
+1. Prepare a CSV file containing a `doi` column, e.g.:
+   ```csv
+   doi
+   10.1145/3706598.3714276
+    10.1145/3706598.3714095
+    10.1145/3706598.3713835
+   ```
+
+2. Run the script:
+   ```bash
+   python data_extraction.py
+   ```
+
+3. The results will be saved in:
+   ```
+   files_output/data_test.json
+   ```
+
+Example output:
+
+```json
+{
+  "10.1145/3706598.3714276": {
+    "venue": "Conference on Human Factors in Computing Systems Proceedings",
+    "year": "2025",
+    "title": "PCB Renewal: Iterative Reuse of PCB Substrates for Sustainable Electronic Making",
+    "citedby_count": "2"
+  }
+}
+```
+
+### Important
+- Requires an active **Elsevier API Key** (X-ELS-APIKey) and (in most cases) **Institution Token** (X-ELS-Insttoken)
+    If you are accessing the API outside your institution’s network, you may need to include the Institution Token to authenticate.
+- Rate limits imposed by Elsevier may apply
--- data_extraction.py
+++ data_extraction.py
@@ -1,41 +1,136 @@
 import requests
+from dataclasses import dataclass
+from typing import Callable, List, Dict, Protocol
+import pandas as pd
+import json
 
-def title_year(paper):
-    if 'message' in paper:
-        try:
-            if 'published-print' in paper['message']:
-                year= paper['message']['published-print']['date-parts'][0][0]  
-            elif 'created' in paper['message']:
-                    year= paper['message']['created']['date-parts'][0][0]
-            else: year='-'
-        except Exception as e:
-            print(f"Error fetching year for {paper['message']['DOI']}: {str(e)}")
-            year='-'
-        try:
-            if 'title' in paper['message']:
-                title=paper['message']['title'][0]
-            else: title='-'
-        except Exception as e:
-            print(f"Error fetching title for {paper['message']['DOI']}: {str(e)}")
-            title='-'
-    else:
-        return '-','-'
-    return title,year  
+API_KEY = '' #your API key
+Insttoken='' #your Institution Token
 
-def references(paper,doi):
-    try:
-        if paper and 'message' in paper and 'reference' in paper['message']:
-            return [ref.get('DOI') for ref in paper['message']['reference'] if ref.get('DOI')]
-    except Exception as e:
-        print(f"Error fetching references for {doi}: {str(e)}")
-    return []
+HEADERS = {
+    "Accept": "application/json",
+    "X-ELS-APIKey": API_KEY,
+    "X-ELS-Insttoken":Insttoken
+}
+
+@dataclass
+class Config:
+    input_file: str
+    output_file:str
+
+class InputReader(Protocol):
+    def read(self, file: str) -> List: ...
+
+
+class CSVReader:
+    def read(self, file: str) -> List:
+        dois=pd.read_csv(file)
+        return list(dois['doi'])
+
+class JSONReader:
+    def read(self, file: str) -> List:
+        with open(file,'r') as f:
+            data=json.load(f)
+        return list(data.keys())
     
-def get_data(doi):
+class JSONResultWriter():
+    def write(self, report: Dict[str, object], output_file: str) -> None:
+        with open(output_file, "w") as f:
+            json.dump(report, f, indent=2)
+
+class MetadataExtractor:
+    def __init__(
+        self, reader: InputReader, metrics: List[Callable]
+    ):
+        self.reader = reader
+        self.metrics = metrics
+
+    def extract(self, config: Config) -> Dict[str, object]:
+        dois = self.reader.read(config.input_file)
+        result={}
+        for i, doi in enumerate(dois, 1):
+            print(f"[{i}/{len(dois)}] Processing DOI: {doi}")
+            metadata = {}
+            url=f"https://api.elsevier.com/content/search/scopus?query=DOI({doi})"
+            entries=get_work(doi,url)["search-results"]["entry"]
+            for metric in self.metrics:
+                metadata.update(metric(entries))
+            result[doi]=metadata
+        return result
+    
+def get_work(doi:str,url:str) -> Dict:
     try:
-        url = f"https://api.crossref.org/works/{doi}"
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
+        r = requests.get(url, headers=HEADERS)
+        r.raise_for_status()
+        data = r.json()
         return data
     except Exception as e:
-        print(f"Error fetching data for {doi}: {str(e)}")
+        print(f"Error fetching work for {doi}: {str(e)}")
+        return {}
+
+def get_publicationName(entries:Dict) -> Dict[str, str]:
+    try:
+        if entries:
+            return {'venue':entries[0]['prism:publicationName']}
+        else:
+            return {'venue':'-'}
+    except Exception as e:
+        print(f"Error fetching publicationName: {str(e)}")
+        return {'venue':'-'}
+    
+def get_year(entries:Dict) -> Dict[str, str]:
+    try:
+        if entries:
+            return {'year':entries[0]['prism:coverDate'][:4]}
+        else:
+            return {'year':'-'}
+    except Exception as e:
+        print(f"Error fetching year: {str(e)}")
+        return {'year ':'-'}
+    
+def get_title(entries:Dict) -> Dict[str, str]:
+    try:
+        if entries:
+            return {'title':entries[0]['dc:title']}
+        else:
+            return {'title':'-'}
+    except Exception as e:
+        print(f"Error fetching title: {str(e)}")
+        return {'title':'-'}
+
+def get_citedbyCount(entries:Dict) -> Dict[str, str]:
+    try:
+        if entries:
+            return {'citedby_count':entries[0]['citedby-count']}
+        else:
+            return {'citedby_count':'-'}
+    except Exception as e:
+        print(f"Error fetching citedbyCount: {str(e)}")
+        return {'citedby_count':'-'}
+
+def get_refDOIs(entries:Dict) -> Dict[str, str]:
+    #to do
+    pass
+
+def main():
+    config = Config(
+        input_file="files_input/dois_test.csv",
+        output_file='files_output/data_test.json'
+    )
+    preprocessing_steps = [
+        get_publicationName,
+        get_year,
+        get_title,
+        get_citedbyCount
+    ]
+    reader = CSVReader()
+    if not API_KEY:
+        raise ValueError("API_KEY and/or Insttoken must be set before running.")
+    else:
+        extractor = MetadataExtractor(reader, preprocessing_steps)
+        report = extractor.extract(config)
+        writer = JSONResultWriter()
+        writer.write(report, config.output_file)
+
+if __name__ == "__main__":
+    main()
